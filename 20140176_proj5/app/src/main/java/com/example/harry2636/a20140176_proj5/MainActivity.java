be118c691a51505dac28362314127a57591269d0
harry2636 <harry2636@naver.com>
2016-12-09 01:16:21 +0900

    Added UI component.

--- 20140176_proj5/app/src/main/java/com/example/harry2636/a20140176_proj5/MainActivity.java
+++ 20140176_proj5/app/src/main/java/com/example/harry2636/a20140176_proj5/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.harry2636.a20140176_proj5;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    Button fileChooser = (Button)findViewById(R.id.file_choose_button);
+    fileChooser.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Intent fileChooserIntent = new Intent(MainActivity.this, DocumentsSample.class);
+        startActivity(fileChooserIntent);
+      }
+    });
   }
 }
